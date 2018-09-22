f3a9a062bffdebc285f3638bc641e40c9feb6ff8
Simon Rojas <simonjrojas@hotmail.es>
2018-09-22 02:05:04 -0400

    Routing Added - Part 1

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -8,27 +8,37 @@ import { HeroService } from '../hero.service';
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
+
 export class HeroesComponent implements OnInit {
-
-  //heroes = HEROES;
   heroes: Hero[];
-  selectedHero: Hero;
-
 
   constructor(private heroService: HeroService) { }
 
   ngOnInit() {
-  this.getHeroes();
-  }
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
+    this.getHeroes();
   }
 
   getHeroes(): void {
-  this.heroService.getHeroes()
-      .subscribe(heroes => this.heroes = heroes);
+    this.heroService.getHeroes()
+    .subscribe(heroes => this.heroes = heroes);
   }
-  //getHeroes(): void {
-  //this.heroes = this.heroService.getHeroes();
-  //}
 }
+
+
+//   constructor(private heroService: HeroService) { }
+
+//   ngOnInit() {
+//   this.getHeroes();
+//   }
+//   onSelect(hero: Hero): void {
+//     this.selectedHero = hero;
+//   }
+
+//   getHeroes(): void {
+//   this.heroService.getHeroes()
+//       .subscribe(heroes => this.heroes = heroes);
+//   }
+//   //getHeroes(): void {
+//   //this.heroes = this.heroService.getHeroes();
+//   //}
+// }
